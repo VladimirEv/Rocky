638f601970ce0bfa83e4302d15cb7c01371ec855
Vladimir Belevich <vladimir.ev.belevich@gmail.com>
2022-04-25 23:53:26 +0300

    Created metod "Edit"

--- Rocky/Controllers/CategoryController.cs
+++ Rocky/Controllers/CategoryController.cs
@@ -39,5 +39,23 @@ namespace Rocky.Controllers
             }
             return View(obj);
         }
+
+        //Get - Edit;  GET-запросы, это те запросы которые возвращают View
+        public IActionResult Edit(int id)
+        {
+            if (id == null || id==0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Category.Find(id);
+
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
     }
 }
