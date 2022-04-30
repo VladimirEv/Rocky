3b4fedb95983e79b3bcda2ee2f9b8da62140b2b6
Vladimir Belevich <vladimir.ev.belevich@gmail.com>
2022-04-30 23:15:56 +0300

    Created model "Product" and created table "Product" in DB

---
+++ Rocky/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Rocky.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Range(1,int.MaxValue)]
+        public double Price { get; set; }
+        public string Image { get; set; }
+
+        [Display(Name="Category Type")]
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]                       //Чтобы установить свойство в качестве внешнего ключа, применяется атрибут [ForeignKey]:
+        public virtual Category Category { get; set; }
+
+    }
+}
