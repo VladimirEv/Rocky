0418b8982fd33cfff1e7848a419889db52f5e33e
Vladimir Belevich <vladimir.ev.belevich@gmail.com>
2022-05-09 14:16:06 +0300

    Added class "HomeVM" (proporties: "Products", "Categories")

--- Rocky/Controllers/HomeController.cs
+++ Rocky/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace Rocky.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Work()
+        {
+            return View();
+        }
     }
 }
---
+++ Rocky/Models/ViewModels/HomeVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Models.ViewModels
+{
+    public class HomeVM
+    {
+        IEnumerable<Product> Products { get; set; }
+        IEnumerable<Category> Categories { get; set; }
+    }
+}
