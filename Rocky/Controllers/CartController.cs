70a07297534d96ee48320141742bebf5e2d911c1
Vladimir Belevich <vladimir.ev.belevich@gmail.com>
2022-06-26 19:40:42 +0300

    Confirmetion requiest

--- Rocky/Controllers/CartController.cs
+++ Rocky/Controllers/CartController.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Rocky.Data;
 using Rocky.Models;
 using Rocky.Models.ViewModels;
 using Rocky.Utility;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Claims;
+using System.Text;
 
 namespace Rocky.Controllers
 {
@@ -15,12 +19,19 @@ namespace Rocky.Controllers
     public class CartController : Controller
     {
         private readonly ApplicationDbContext _db;
+
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        private readonly IEmailSender _emailSender;
+
         [BindProperty]                                   //привяжем свойство для POST запросов; и теперь нам не нужно указывать это свойство в качестве параметра ЯВНО
         public ProductUserVM ProductUserVM { get; set; }
 
-        public CartController(ApplicationDbContext db)
+        public CartController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment, IEmailSender emailSender)
         {
             _db = db;
+            _webHostEnvironment = webHostEnvironment;
+            _emailSender = emailSender;
         }
 
         public IActionResult Index()
@@ -77,10 +88,55 @@ namespace Rocky.Controllers
             ProductUserVM = new ProductUserVM()
             {
                 ApplicationUser = _db.ApplicationUser.FirstOrDefault(u => u.Id == claim.Value),
-                ProductList = prodList
+                ProductList = prodList.ToList()
             };
 
             return View(ProductUserVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Summary")]
+        public IActionResult SummaryPost(ProductUserVM ProductUserVM)
+        {
+            var PathToTemplate = _webHostEnvironment.WebRootPath + Path.DirectorySeparatorChar.ToString()   //Path.DirectorySeparatorChar.ToString() - наклонная черта
+                + "templates" + Path.DirectorySeparatorChar.ToString() +
+                "Inquiry.html";
+
+            var subject = "New Inquiry";
+            string HtmlBody = "";
+
+            using(StreamReader sr = System.IO.File.OpenText(PathToTemplate))
+            {
+                HtmlBody = sr.ReadToEnd(); //прочитаем и сохраним шаблон в объект HtmlBody
+            }
+
+            //Name: {0}
+            //Email: {1}
+            //Phone: {2}
+            //Products: {3}
+
+            StringBuilder productListSB = new StringBuilder();
+            foreach (var prod in ProductUserVM.ProductList)
+            {
+                productListSB.Append($" - Name: {prod.Name} <span style='font-size:14px;'>(ID: {prod.Id})</span><br />");
+            }
+
+            string messageBody = string.Format(HtmlBody,
+                ProductUserVM.ApplicationUser.FullName,
+                ProductUserVM.ApplicationUser.Email,
+                ProductUserVM.ApplicationUser.PhoneNumber,
+                productListSB.ToString()
+                );
+
+            return RedirectToAction(nameof(InquiryConfirmation));
+        }
+
+
+        public IActionResult InquiryConfirmation()
+        {
+            HttpContext.Session.Clear(); //очистим данные текущей сессии. Для текущей сессии все товары, которые интересовали клиента, уже попали в запрос
+            return View();
         }
 
 
