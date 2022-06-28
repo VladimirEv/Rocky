c779fb221956c7b47e5279f449e2fb2ac5506bc5
Vladimir Belevich <vladimir.ev.belevich@gmail.com>
2022-06-28 13:38:21 +0300

    Created View for "PileCalculation"

---
+++ Rocky/Models/SoilProperties.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rocky.Models
+{
+    public class SoilProperties
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int NumberOfSoil { get; set; }                   //номер грунта по геологии
+
+        public string Name { get; set; }
+
+        public string SoilStrengthForSand { get; set; }        //прочный ...
+
+        public double PorosityCoefficient { get; set; }        //e
+
+        public double YieldRate { get; set; }                  // IL
+
+        public double AngleOfInternalFriction { get; set; }    // фи
+
+        public double SpecificAdhesion { get; set; }           // с
+
+        public double DeformationModulus { get; set; }         // E
+
+    }
+}
