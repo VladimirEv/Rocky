11873169b7fcc79d7a6dcb4103b89b0066585e42
Vladimir Belevich <vladimir.ev.belevich@gmail.com>
2022-07-16 23:54:35 +0300

    Created "InquiryController". Created model "Inquiry"

--- Rocky/Controllers/InquiryController.cs
+++ Rocky/Controllers/InquiryController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Rocky_DataAccess.Repository.IRepository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,15 @@ namespace Rocky.Controllers
 {
     public class InquiryController : Controller   //код для вывода заголовка запроса
     {
+        private readonly IInquiryHeaderRepository _inqHRepo;
+        private readonly IInquiryDetailRepository _inqDRepo;
+
+        public InquiryController(IInquiryHeaderRepository inqHRepo, IInquiryDetailRepository inqDRepo)
+        {
+            _inqHRepo = inqHRepo;
+            _inqDRepo = inqDRepo;
+        }
+
         public IActionResult Index()
         {
             return View();
